dfa1ca58e0a4cdaead468e707d69d4bd15adc823
Sneedev <77446897+Sneedev@users.noreply.github.com>
2021-02-06 13:24:43 +0000

    First Commit

---
+++ Assets/DataFiles/Scripts/PortalDoor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material[] PGMat;
+    void Start()
+    {
+        foreach (Material mat in PGMat)
+        {
+            mat.SetInt("stest", (int)CompareFunction.Equal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider collide)
+    {
+        if (collide.gameObject.CompareTag("MainCamera"))
+        {
+            if (transform.position.z > collide.transform.position.z)
+            {
+                foreach (Material mat in PGMat)
+                {
+                    mat.SetInt("stest", (int)CompareFunction.Equal);
+                }
+            }
+            else
+            {
+                foreach (Material mat in PGMat)
+                {
+                    mat.SetInt("stest", (int)CompareFunction.NotEqual);
+                }
+            }
+        }
+    }
+}
